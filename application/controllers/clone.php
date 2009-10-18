c6076003a2de11a1a57c0321f712c2f01d499116
eagle0527 <ealge0527@gmail.com>
2009-10-18 15:05:06 +0800

    clone test begin

---
+++ application/controllers/clone.php
@@ -0,0 +1,29 @@
+<?php defined('SYSPATH') OR die('No direct access allowed.');
+
+class Test_Controller extends Controller {
+    public function __construct()
+    {
+        parent::__construct(); // This must be included
+
+        $this->session = Session::instance();
+		$this->profiler = new Profiler;
+    }
+
+
+
+	public function index()
+	{
+
+			$this->session->set('session_test','test');
+			echo 'session set test';
+
+			echo 'clone test';
+	}
+
+	public function get()
+	{
+
+			$result	= $this->session->get('session_test');
+			echo $result;
+	}
+} 
